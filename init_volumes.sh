62f067fe15432be43ff8e31fce9e4e289c76f53a
Marco Ardizzone <kwbmm1990@gmail.com>
2025-10-12 01:40:47 +0200

    Removed Ryot and Changedetection

--- init_volumes.sh
+++ init_volumes.sh
@@ -16,8 +16,6 @@ mkdir -p volumes/invoiceninja-db
 mkdir -p volumes/invoiceshelf/data
 mkdir -p volumes/invoiceshelf/conf
 mkdir -p volumes/invoiceshelf-db
-mkdir -p volumes/ryot-db
-mkdir -p volumes/changedetection
 mkdir -p volumes/hoarder
 mkdir -p volumes/mealie
 mkdir -p volumes/mealie-db
