844b2bf57d078c45e49a6492d2a76925259b7615
Marco Ardizzone <kwbmm1990@gmail.com>
2025-12-27 15:47:46 +0100

    Update invoiceninja

--- compose/invoiceninja/scripts/init.sh
+++ compose/invoiceninja/scripts/init.sh
@@ -7,9 +7,10 @@ elif [ "$(dpkg --print-architecture)" = "arm64" ]; then
     export SNAPPDF_CHROMIUM_PATH=/usr/bin/chromium
 fi
 
-if [ "$*" = 'supervisord -c /etc/supervisor/conf.d/supervisord.conf' ]; then
+if [ "$*" = 'supervisord -c /etc/supervisor/supervisord.conf' ]; then
 
     # Check for required folders and create if needed
+    [ -d /var/www/html/storage/app/public ] || mkdir -p /var/www/html/storage/app/public
     [ -d /var/www/html/storage/framework/sessions ] || mkdir -p /var/www/html/storage/framework/sessions
     [ -d /var/www/html/storage/framework/views ] || mkdir -p /var/www/html/storage/framework/views
     [ -d /var/www/html/storage/framework/cache ] || mkdir -p /var/www/html/storage/framework/cache
@@ -20,10 +21,11 @@ if [ "$*" = 'supervisord -c /etc/supervisor/conf.d/supervisord.conf' ]; then
         rm -rf /var/www/html/public/.htaccess \
             /var/www/html/public/.well-known \
             /var/www/html/public/*
-        mv /tmp/public/* \
+        cp -r /tmp/public/* \
             /tmp/public/.htaccess \
             /tmp/public/.well-known \
-            /var/www/html/public/
+            /var/www/html/public/ && \
+            rm -rf /tmp/public/*
     fi
     echo "Public Folder is up to date"
 
@@ -40,24 +42,25 @@ if [ "$*" = 'supervisord -c /etc/supervisor/conf.d/supervisord.conf' ]; then
 
     # Clear and cache config in production
     if [ "$APP_ENV" = "production" ]; then
+        runuser -u www-data -- php artisan migrate --force       
+        runuser -u www-data -- php artisan cache:clear # Clear after the migration
+        runuser -u www-data -- php artisan ninja:design-update
         runuser -u www-data -- php artisan optimize
-        runuser -u www-data -- php artisan package:discover
-        runuser -u www-data -- php artisan migrate --force
 
         # If first IN run, it needs to be initialized
-        if [ "$(php -d opcache.preload='' artisan tinker --execute='echo Schema::hasTable("accounts") && !App\Models\Account::all()->first();')" = "1" ]; then
+        if [ "$(runuser -u www-data -- php artisan tinker --execute='echo Schema::hasTable("accounts") && !App\Models\Account::all()->first();')" = "1" ]; then
             echo "Running initialization..."
-
-            php artisan db:seed --force
+            
+            runuser -u www-data -- php artisan db:seed --force
 
             if [ -n "${IN_USER_EMAIL}" ] && [ -n "${IN_PASSWORD}" ]; then
-                php artisan ninja:create-account --email "${IN_USER_EMAIL}" --password "${IN_PASSWORD}"
+                runuser -u www-data -- php artisan ninja:create-account --email "${IN_USER_EMAIL}" --password "${IN_PASSWORD}"
             else
                 echo "Initialization failed - Set IN_USER_EMAIL and IN_PASSWORD in .env"
                 exit 1
             fi
-        fi
 
+        fi
         echo "Production setup completed"
     fi
 
